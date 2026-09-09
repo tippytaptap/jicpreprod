477149122bbcacaec8ed19cdbe6eec94a9da2abf
tippytaptap <haider194ali@gmail.com>
2026-09-09 02:34:51 +0100

    Add working Madrassah sub-tab anchors

--- src/pages/MadrassahPage.jsx
+++ src/pages/MadrassahPage.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-    import HeroSection from '@/components/sections/madrassah/HeroSection';
-    import AboutSection from '@/components/sections/madrassah/AboutSection';
-    import ProgramsSection from '@/components/sections/madrassah/ProgramsSection';
-    import SpecialCoursesSection from '@/components/sections/madrassah/SpecialCoursesSection';
-    import EnrollmentSection from '@/components/sections/madrassah/EnrollmentSection';
-    import TestimonialsSection from '@/components/sections/madrassah/TestimonialsSection';
-    import CallToActionSection from '@/components/sections/madrassah/CallToActionSection';
-    import EnrollmentForm from '@/components/sections/madrassah/EnrollmentForm';
+import HeroSection from '@/components/sections/madrassah/HeroSection';
+import AboutSection from '@/components/sections/madrassah/AboutSection';
+import ProgramsSection from '@/components/sections/madrassah/ProgramsSection';
+import SpecialCoursesSection from '@/components/sections/madrassah/SpecialCoursesSection';
+import TestimonialsSection from '@/components/sections/madrassah/TestimonialsSection';
+import CallToActionSection from '@/components/sections/madrassah/CallToActionSection';
+import EnrollmentForm from '@/components/sections/madrassah/EnrollmentForm';
 
-    const MadrassahPage = () => {
-      return (
-        <div className="page-transition pt-24">
-          <HeroSection />
-          <AboutSection />
-          <ProgramsSection />
-          <SpecialCoursesSection />
-          <section id="enrollment" className="py-16 bg-gray-50 dark:bg-gray-800">
-            <div className="container mx-auto px-4">
-              <EnrollmentForm />
-            </div>
-          </section>
-          <TestimonialsSection />
-          <CallToActionSection />
+const MadrassahPage = () => {
+  return (
+    <div className="page-transition pt-24">
+      <HeroSection />
+      <AboutSection />
+      <div id="classes" className="jic-anchor-target"><ProgramsSection /></div>
+      <div id="special-courses" className="jic-anchor-target"><SpecialCoursesSection /></div>
+      <section id="enrollment" className="jic-anchor-target py-16 bg-gray-50 dark:bg-gray-800">
+        <div className="container mx-auto px-4">
+          <EnrollmentForm />
         </div>
-      );
-    };
+      </section>
+      <TestimonialsSection />
+      <CallToActionSection />
+    </div>
+  );
+};
 
-    export default MadrassahPage;
+export default MadrassahPage;
