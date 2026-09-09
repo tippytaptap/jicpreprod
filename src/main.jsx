ad7f186ef3cf8132be898fe92a24ebb248c0abf0
tippytaptap <haider194ali@gmail.com>
2026-09-09 03:33:28 +0100

    Load restructure glass styles

--- src/main.jsx
+++ src/main.jsx
@@ -31,6 +31,7 @@ showStatus('Loading website…');
       import('@/styles/mobile-fixes.css'),
       import('@/styles/ios-glass.css'),
       import('@/styles/final-touches.css'),
+      import('@/styles/jic-restructure.css'),
     ]);
 
     const React = ReactModule.default;
